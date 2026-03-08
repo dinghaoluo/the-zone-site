5ddb4f8067bad8363fd2bdadd66d3311f59e9283
dinghaoluo <dinghao.luo@outlook.com>
2026-03-08 11:05:22 -0400

    data: homepage language summary

---
+++ src/data/homepageLanguageSummary.ts
@@ -0,0 +1,7 @@
+export const homepageLanguageSummary = [
+  { key: 'German', colour: '--language-german', total: 411, legible: 356 },
+  { key: 'French', colour: '--language-french', total: 87, legible: 86 },
+  { key: 'Russian (transliterated)', colour: '--language-russian', total: 21, legible: 21 },
+  { key: 'Herero/Nama', colour: '--language-herero', total: 15, legible: 15 },
+  { key: 'Latin', colour: '--language-latin', total: 5, legible: 5 },
+] as const;
