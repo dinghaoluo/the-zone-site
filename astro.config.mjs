812fb997c3b809dba31aec64a400de721a4ce70a
dinghaoluo <dinghao.luo@outlook.com>
2026-08-04 12:40:53 -0400

    feat: add sitemap and canonical urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dinghaoluo.github.io',
   base: '/the-zone-site',
   output: 'static',
+  integrations: [sitemap()],
 });
